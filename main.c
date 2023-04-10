265daa86e458f72e114934cbeaa9ff37338bcaad
Miguel Perez <miguepersa@gmail.com>
2023-04-10 16:39:12 -0400

    commit inicial - parsing de argumentos iniciado

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <getopt.h>
+
+int main(int argc, char const *argv[])
+{
+    int opt;
+    int count_flag = 1;
+    int list_flag = 0;
+    int size_flag = 0;
+    char *region = NULL;
+    char *species = NULL;
+    char *type = NULL;
+    char *name = NULL;
+
+    static struct option long_options[] = {
+        {"nocount", no_argument, 0, 'c'},
+        {"list", no_argument, 0, 'l'},
+        {"size", no_argument, 0, 's'},
+        {"region", required_argument, 0, 'r'},
+        {"species", required_argument, 0, 'p'},
+        {"type", required_argument, 0, 't'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "clr:p:t:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'c':
+                count_flag = 0;
+                break;
+            case 'l':
+                list_flag = 1;
+                break;
+            case 's':
+                size_flag = 1;
+                break;
+            case 'r':
+                region = optarg;
+                break;
+            case 'p':
+                species = optarg;
+                break;
+            case 't':
+                type = optarg;
+                break;
+            case '?':
+                // handle invalid option
+                break;
+            default:
+                // handle other cases
+                break;
+        }
+    }
+
+    if (optind < argc) {
+        name = argv[optind];
+    }
+
+    // process arguments here
+
+    return 0;
+}
